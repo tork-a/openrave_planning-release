2aa6e35bfa74eba0dd390203e847d9be58ba0c54
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-02-15 09:55:58 +0900

    Revert "Trimmed the branch to only the openrave_planning sub directory"
    This reverts commit cb50995f261e67acee67621945810cba3e95ecd6.

---
+++ openrave/CHANGELOG.rst
@@ -0,0 +1,29 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package openrave
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.0.5 (2017-02-03)
+------------------
+* re-enable opende in package.xml
+* 99.openarve.sh.in: openrave-config is now global executive
+* use production release of Jan/19/2017
+* Contributors: Kei Okada
+
+0.0.4 (2017-01-19)
+------------------
+* use dist-packages as python install dir instad of site-packages
+* Contributors: Kei Okada
+
+0.0.3 (2016-05-27)
+------------------
+* CMakeLists.txt : fix compiile scheme for install/devel
+* Contributors: Kei Okada
+
+0.0.2 (2016-05-25)
+------------------
+
+0.0.1 (2016-05-25)
+------------------
+* add 99.openrave.sh
+* update openrave/collada_robots package to catkin
+* Contributors: Kei Okada
---
+++ openrave_robot_control/testopenravecontrol.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from __future__ import with_statement # for python 2.5
+PKG = 'openrave_robot_control'
+import roslib; roslib.load_manifest(PKG)
+
+from openravepy import *
+from numpy import *
+import time
+import sys
+
+env = Environment() # create openrave environment
+env.SetViewer('qtcoin')
+env.Load(sys.argv[1])
+with env:
+    robot = env.GetRobots()[0] # get the first robot
+    manip = robot.GetManipulators()[0]
+    jointnames = ' '.join(robot.GetJoints()[j].GetName() for j in manip.GetArmJoints())
+    robot.SetController(RaveCreateController(env,'ROSOpenRAVE trajectoryservice /controller_session '+jointnames),range(robot.GetDOF()),0)
+    lower,upper = robot.GetDOFLimits()
+
+# sending velocity command?
+#robot.GetController().SendCommand("setvelocity 4 .01")
+
+basemanip = interfaces.BaseManipulation(robot)
+i = 0
+while True:
+    with robot: # save the robot state and get random joint values that are collision free
+        while True:
+            values = lower + random.rand(len(lower))*(upper-lower)
+            robot.SetJointValues(values)
+            if not robot.CheckSelfCollision() and not env.CheckCollision(robot):
+                break
+    print 'setting: ',values
+    if i%2:
+        print 'planner ',values
+        basemanip.MoveActiveJoints(values,outputtrajobj=True)
+    else:
+        print 'SetDesired ',values
+        robot.GetController().SetDesired(values)
+    robot.WaitForController(0)
+    time.sleep(1.0)
+    i += 1
+
+RaveDestroy()
